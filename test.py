a0b8610b34a80f825fb2f1d8c48ab45bd1dcd873
zhangjiuye <zhangyejiujesse@outlook.com>
2024-11-10 09:49:50 +0800

    'change'

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import test2
+import math
 print("Hello World")
 def helotest():
     print("Hello function test")
+
+ 
+print(math.sqrt(4)  )
 if __name__ == "__main__":
     helotest()
     test2.helo()
--- test2.py
+++ test2.py
@@ -1,3 +1,6 @@
 print("Hello test2")
 def helo():
     print("Hello function test2")
+
+if __name__ == "__main__":
+    helo()    
